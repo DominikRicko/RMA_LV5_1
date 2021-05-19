235037814c9fc9c6664c8b60e7ea50551cf1178e
Dominik Ričko <dominik.ricko@gmail.com>
2021-05-19 09:29:41 +0200

    added application context

--- app/src/main/java/hr/dominikricko/rma_lv5_1/ui/MainActivity.kt
+++ app/src/main/java/hr/dominikricko/rma_lv5_1/ui/MainActivity.kt
@@ -1,11 +1,15 @@
-package hr.dominikricko.rma_lv5_1
+package hr.dominikricko.rma_lv5_1.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import hr.dominikricko.rma_lv5_1.R
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+
 }
